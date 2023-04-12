2c95ea50d7976cece48580f50c620bfae0af96c1
endercan ergin <87834304+enderergin@users.noreply.github.com>
2023-04-13 00:46:24 +0300

    Proje dosyası ekle.

---
+++ Matematik.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    public class Matematik
+    {
+        public void menu() 
+        {
+            Console.Clear();
+            Console.WriteLine("---MENÜ----");
+            Console.WriteLine("Toplama işlemi için 1. basın");
+            Console.WriteLine("Çıkarma işlemi için 2. basın");
+            Console.WriteLine("Çapma işlemi için 3. basın");
+            Console.WriteLine("Bolme işlemi için 4. basın");
+            
+        }
+        public decimal topla (decimal say1, decimal say2) 
+        {
+            decimal sonuc = say1 + say2;
+            return sonuc;
+        }
+        public decimal cikar(decimal say1, decimal say2)
+        {
+            decimal sonuc = say1 - say2;
+            return sonuc;
+        }
+        public decimal carp(decimal say1, decimal say2)
+        {
+            decimal sonuc = say1 * say2;
+            return sonuc;
+        }
+        public decimal bol(decimal say1, decimal say2)
+        {
+            decimal sonuc = say1 / say2;
+            return sonuc;
+        }
+        public void cikti (decimal say1, decimal say2, decimal sonuc, string opterador)
+        {
+            Console.WriteLine("{0} {1} {2} = {3}", say1,say2,opterador,sonuc);
+        }
+        public void dongu()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematik m = new Matematik();
+            basadon:
+            m.menu();
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1.degeri girin");
+            decimal say1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2.degeri girin");
+            decimal say2 = Convert.ToInt32(Console.ReadLine());
+
+            decimal sonuc = 0;
+
+
+            switch (s)
+            {
+                case 1:
+                   sonuc = m.topla(say1, say2);
+                    m.cikti(say1, say2, sonuc, "+");
+                    break;
+                case 2:
+                   sonuc = m.cikar(say1, say2);
+                    m.cikti(say1, say2, sonuc, "-");
+                    break;
+                case 3:
+                   sonuc = m.carp(say1, say2);
+                    m.cikti(say1, say2, sonuc, "*");
+
+                    break;
+                case 4:
+                   sonuc = m.bol(say1, say2);
+                    m.cikti(say1,say2,sonuc,"/");
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz işlem seçtiniz");
+                    Console.WriteLine("Yeniden deneyin!");
+                    goto basadon;
+                    
+
+            }
+            Console.WriteLine("Başa dönmek için (E/H)");
+            string d = Console.ReadLine();
+            if (d.ToUpper() == "E")
+            {
+                goto basadon;
+            }
+        }
+    }
+}
